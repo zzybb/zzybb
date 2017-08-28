68d5a293a142890b716fa2fe58467bb33711cd84
zzybb <576915249+zzybb@users.noreply.github.com>
2017-08-28 18:53:28 +0800

    sss
    dddd

--- Scripts/firstpage.js
+++ Scripts/firstpage.js
@@ -1,5 +1,6 @@
 addLoadEvent(nav);
 addLoadEvent(hideshow);
+addLoadEvent(shift);
 
 function nav() {
     var nav = document.getElementById("nav");
@@ -24,7 +25,7 @@ function navseult(whicnav) {
 
     }
     if (whicnav == links[1]) {
-        nav3[0].style.left = "940px";
+        nav3[0].style.left = "972px";
         nav2[0].style.left = "-9999px";
         links[1].style.backgroundColor = "white";
         links[1].style.color = "black";
@@ -62,3 +63,91 @@ function hideshow() {
         }
     }
 }
+
+function shift() {
+    var select = document.getElementsByClassName("select");
+    var selecta = select[0].getElementsByTagName("a");
+    for (var i = 0; i < selecta.length; i++) {
+        selecta[i].i = i;
+        selecta[i].onclick = function () {
+            slowchange(100, this.i);
+            showPic(this);
+            return false;
+        }
+    }
+}
+
+function showPic(whichse) {
+    var select = document.getElementsByClassName("select");
+    var selecta = select[0].getElementsByTagName("a");
+    var showPic = document.getElementsByClassName("showpic");
+    var selectall = showPic[0].getElementsByTagName("div");
+    if (whichse == selecta[0]) {
+        selecta[0].setAttribute("id", "selectred");
+        selecta[1].setAttribute("id", "");
+        selecta[2].setAttribute("id", "");
+        selecta[3].setAttribute("id", "");
+        selectall[0].style.display = "block";
+        selectall[1].style.display = "none";
+        selectall[2].style.display = "none";
+        selectall[3].style.display = "none";
+    }
+    if (whichse == selecta[1]) {
+        selecta[1].setAttribute("id", "selectred");
+        selecta[0].setAttribute("id", "");
+        selecta[2].setAttribute("id", "");
+        selecta[3].setAttribute("id", "");
+        selectall[1].style.display = "block";
+        selectall[0].style.display = "none";
+        selectall[2].style.display = "none";
+        selectall[3].style.display = "none";
+    }
+    if (whichse == selecta[2]) {
+        selecta[2].setAttribute("id", "selectred");
+        selecta[1].setAttribute("id", "");
+        selecta[0].setAttribute("id", "");
+        selecta[3].setAttribute("id", "");
+        selectall[2].style.display = "block";
+        selectall[0].style.display = "none";
+        selectall[1].style.display = "none";
+        selectall[3].style.display = "none";
+    }
+    if (whichse == selecta[3]) {
+        selecta[3].setAttribute("id", "selectred");
+        selecta[1].setAttribute("id", "");
+        selecta[2].setAttribute("id", "");
+        selecta[0].setAttribute("id", "");
+        selectall[3].style.display = "block";
+        selectall[0].style.display = "none";
+        selectall[1].style.display = "none";
+        selectall[2].style.display = "none";
+    }
+}
+
+function slowchange(target,which) {
+    var timer = null;
+    clearInterval(timer);
+    var showPic = document.getElementsByClassName("showpic");
+    var selectall = showPic[0].getElementsByTagName("div");
+    var speed = 1;
+    var alpha = 0;
+    timer = setInterval(function () {
+        if (alpha < target) {
+            speed = 2;
+        } else {
+            speed = -2;
+        }
+        if (alpha == target) {
+            clearInterval(timer);
+        } else {
+            alpha = alpha + speed;
+            selectall[which].style.filter = 'alpha(opacity=' + alpha + ')';
+            selectall[which].style.opacity = alpha / 100;
+        }
+    }, 20);
+}
+
+function marchine() {
+
+}
+
